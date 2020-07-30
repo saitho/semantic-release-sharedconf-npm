fdfcc8f656d68f1ba1b30f89d0e629c0053efa28
Mario Lubenka <mario.lubenka@googlemail.com>
2020-07-30 09:45:47 +0200

    feat: generate Changelog file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
         [
             '@semantic-release/npm',
             {tarballDir: 'release'}
@@ -12,7 +13,7 @@ module.exports = {
         ],
         [
             '@semantic-release/git',
-            {assets: ['package.json']}
+            {assets: ['package.json', 'CHANGELOG.md']}
         ],
         [
             '@semantic-release/github',
